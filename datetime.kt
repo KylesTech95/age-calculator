21025b6495dca2f612358d47291d82c4a29b03af
index-daddy372466 <testmailbox987654@gmail.com>
2025-09-29 10:09:48 -0400

    feat: update datetime with a custom formatter

--- datetime.kt
+++ datetime.kt
@@ -8,6 +8,23 @@ import java.time.Period
 import java.time.format.DateTimeFormatter
 
     fun main () {
-        val today = LocalDate.now()
-        println("Today's date is $today")
+        val current_date = LocalDate.now()
+        println("Today's date is $current_date\n")
+
+        val current_time = LocalTime.now()
+        println("Current time is $current_time\n")
+
+        val current_date_time = LocalDateTime.now()
+        println("Current datetime is $current_date_time\n")
+
+        val zoned_date_time = ZonedDateTime.now()
+        println("Current Zoned datetime is $zoned_date_time\n")
+
+        val specify_date = LocalDate.of(2024,10,12)
+        println("Specificed Date:\n$specify_date\n")
+
+        // val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+        val formatter2 = DateTimeFormatter.ofPattern("dd-MM-yyyy")
+        val formatted_date = current_date.format(formatter2) // plug formatter into current_date
+        println("Formatted current date:\n$formatted_date")
     }
