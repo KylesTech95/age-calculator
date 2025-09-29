5c297ec513050fc42a9792e03651dcb1d58a6473
index-daddy372466 <testmailbox987654@gmail.com>
2025-09-29 09:59:44 -0400

    Initial commit

---
+++ datetime.kt
@@ -0,0 +1,13 @@
+import java.time.LocalDate
+import java.time.LocalTime
+import java.time.LocalDateTime
+import java.time.ZonedDateTime
+import java.time.Instant
+import java.time.Duration
+import java.time.Period
+import java.time.format.DateTimeFormatter
+
+    fun main () {
+        val today = LocalDate.now()
+        println("Today's date is $today")
+    }
